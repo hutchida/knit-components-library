fc67ed120374aa2b60f78b6b3ab972375329f567
Dan <danielmhutchings@gmail.com>
2022-08-18 11:48:48 +0200

    15 | add component description

--- src/components/TweetTile/index.tsx
+++ src/components/TweetTile/index.tsx
@@ -26,6 +26,12 @@ const colorMap = {
   1: { 'heavy': 'orange', 'light': 'peachpuff' },
   2: { 'heavy': 'palegreen', 'light': 'honeydew' },
 }
+/**
+ * TweetTile component that takes config data, user data, and tweet data. The user data is displayed in 
+ * top half of the tile, the tweet data in the lower half. The config data can be used to modify styling
+ * from outside of the component. The tile will change border and background colour like a traffic light, 
+ * based on the sentiment value in the tweet data.
+ */
 export const TweetTile = ({ config, user, tweet }: ITweetTile) => {
   const size = sizeMap[config.size] || '50rem'
   const color = colorMap[tweet.sentiment] || { 'heavy': 'purple', 'light': 'plum' }
