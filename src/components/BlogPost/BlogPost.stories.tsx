0968121bd3f348d121a6a30a6cdd5e5eb38f6b0d
Dan <danielmhutchings@gmail.com>
2022-09-02 11:56:04 +0200

    17 | add image test

--- src/components/BlogPost/BlogPost.stories.tsx
+++ src/components/BlogPost/BlogPost.stories.tsx
@@ -11,7 +11,8 @@ export default {
 const data = {
   subject: 'Welcome to my blog',
   text: `<p>Lorem <strong>ipsum</strong> dolor sit <a href="https://www.bbc.com" target="_blank">amet</a>, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-  <p>Lorem <strong>ipsum</strong> dolor sit <a href="https://www.bbc.com" target="_blank">amet</a>, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>`,
+  <p>Lorem <strong>ipsum</strong> dolor sit <a href="https://www.bbc.com" target="_blank">amet</a>, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+  <img src="images/twitter-profile-pic.jpeg" alt="profile pic" /> Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>`,
   date: '21st Feb 2021',
   info: "For the portfolio",
 }
