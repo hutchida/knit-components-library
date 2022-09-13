da0b1e1e46cba5fe23a10a53a2acef42c5c6c5b4
Dan <danielmhutchings@gmail.com>
2022-09-13 10:10:15 +0200

    add alignment to RichText container

--- src/components/RichText/styles.ts
+++ src/components/RichText/styles.ts
@@ -1,6 +1,9 @@
 import styled from "styled-components"
 
 export const HTMLText = styled.div`
+display: flex;
+justify-content: center;
+flex-direction: column;
 @media (min-width: 768px) {
   max-width: 50vw;
 }
